17396d0697161813f3a9fcd634acbc97808c80f7
abdelfatah hany <aaa5122003@gmail.com>
2024-11-30 23:08:26 +0200

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS books (
     publication_date DATE,
     author_id INT,
     price DECIMAL(10, 2),
-    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+    FOREIGN KEY (author_id) REFERENCES authors(author_id) ON DELETE CASCADE
 );
 
 -- Create the 'customers' table
@@ -37,16 +37,4 @@ CREATE TABLE IF NOT EXISTS orders (
     order_date DATE NOT NULL,
     status VARCHAR(50),
     total_amount DECIMAL(10, 2),
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
-);
-
--- Create the 'order_details' table
-CREATE TABLE IF NOT EXISTS order_details (
-    order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
-    quantity INT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
-);
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE CASCAD
